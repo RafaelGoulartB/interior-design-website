18952e9d019755514db59e9a86afbf5559546110
RafaelGoulartB <rafagoulartb@gmail.com>
2020-09-24 08:58:59 -0300

    🐛 fix: bug with breakpoints

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,10 +1,10 @@
 import { theme, DefaultTheme } from '@chakra-ui/core'
 
 const breakpoints = ["360px", "768px", "1024px", "1440px"];
-breakpoints.sm = breakpoints[0];
-breakpoints.md = breakpoints[1];
-breakpoints.lg = breakpoints[2];
-breakpoints.xl = breakpoints[3];
+// breakpoints.sm = breakpoints[0];
+// breakpoints.md = breakpoints[1];
+// breakpoints.lg = breakpoints[2];
+// breakpoints.xl = breakpoints[3];
 
 const customTheme: DefaultTheme = {
   ...theme,
