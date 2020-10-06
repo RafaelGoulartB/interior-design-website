90f1ca019871f3c5aba7f7dcaa2ca07b79e1b6b2
RafaelGoulartB <rafagoulartb@gmail.com>
2020-10-06 07:41:11 -0300

    📦 feat: add our-projects section

---
+++ components/our-projects.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import { Flex, Text, Heading, Grid, Image } from '@chakra-ui/core'
+
+const OurProject: React.FC = () => {
+  return (
+    <Flex
+      flexDirection="column"
+      alignItems="center"
+      mt={['24', '24', '24', '24', '64']}
+    >
+      <Heading as="h3" textTransform="uppercase" mb="20px">
+        Our Projects
+      </Heading>
+      <Text fontSize="16px" mb="32px" maxW="460px" textAlign="center">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </Text>
+      <Grid
+        gridTemplateColumns="repeat(3, 1fr)"
+        gridGap="26px"
+        px={['8', '8', '8', '24', '24']}
+      >
+        <Image
+          src="/grid/1.png"
+          gridColumn={['1 / 2', '1 / 3', 'auto', 'auto', 'auto']}
+        />
+        <Image
+          src="/grid/2.png"
+          gridColumn={['1 / 2', '2 / 4', 'auto', 'auto', 'auto']}
+        />
+        <Image
+          src="/grid/3.png"
+          gridColumn={['1 / 2', '1 / 3', 'auto', 'auto', 'auto']}
+        />
+        <Image
+          src="/grid/4.png"
+          gridColumn={['1 / 2', '2 / 4', 'auto', 'auto', 'auto']}
+        />
+        <Image
+          src="/grid/5.png"
+          gridColumn={['1 / 2', '1 / 3', 'auto', 'auto', 'auto']}
+        />
+        <Image
+          src="/grid/6.png"
+          gridColumn={['1 / 2', '2 / 4', 'auto', 'auto', 'auto']}
+        />
+      </Grid>
+    </Flex>
+  )
+}
+
+export default OurProject
